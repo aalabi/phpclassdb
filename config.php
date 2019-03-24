2007212e5ea6bd39eb2f70b728c8378ad1715257
Alabi A <alabi10@yahoo.com>
2019-03-24 12:01:22 +0100

    deleted some files

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start();
-	define('SERVER', 'localhost');
-	define('USER', 'root');
-	define('PASSWORD', '');
-	define('DATABASE', 'customers');
-	
--- functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-	function errorMove($error){
-		$_SESSION['error'] = $error;
-		header("Location: error.php");
-		exit;
-	}
